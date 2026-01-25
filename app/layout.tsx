5d75f29036f6730457c209545f8cebb79406f356
jkovacs6001 <jkovacs6001@gmail.com>
2026-01-25 21:04:24 +0000

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NavBarWrapper } from "../components/NavBarWrapper";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +39,7 @@ export default function RootLayout({
             </span>
           </div>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
